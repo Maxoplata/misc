cf83c31ae97bd7cd8c9a877ace7c0bbc65180279
Maxamilian Demian <Maxoplata@users.noreply.github.com>
2021-04-16 08:48:41 -0700

    MosaicMaker.py cleanup

--- MosaicMaker/MosaicMaker.py
+++ MosaicMaker/MosaicMaker.py
@@ -20,79 +20,63 @@ from PIL import Image
 __author__ = "Maxamilian Demian"
 __email__ = "max@maxdemian.com"
 
-error = False
-
 # if we have arguments passed to the script
 if len(sys.argv) != 4:
-	error = 'Invalid argument count'
-else:
-	# vars
-	tileSize = int(sys.argv[1])
-	inputFile = sys.argv[2]
-	outputFile = sys.argv[3]
+	raise Exception('Invalid argument count')
 
-	# validate the tile size
-	if not error:
-		if tileSize < 2:
-			error = 'Invalid tile size (minimum 2)'
+# vars
+tileSize = int(sys.argv[1])
+inputFile = sys.argv[2]
+outputFile = sys.argv[3]
 
-	# validate input file
-	urlData = None
+# validate the tile size
+if tileSize < 2:
+	raise Exception('Invalid tile size (minimum 2)')
 
-	if not error:
-		if not os.path.exists(inputFile):
-			try:
-				urlData = urllib2.urlopen(inputFile).read()
-			except:
-				error = 'File does not exist'
+# validate input file
+urlData = None
 
+if not os.path.exists(inputFile):
+	try:
+		urlData = urllib2.urlopen(inputFile).read()
+	except:
+		raise Exception('File does not exist')
+
+try:
 	# validate image file
-	imgOrig = None
+	imgOrig = Image.open(inputFile) if urlData == None else Image.open(cStringIO.StringIO(urlData))
 
-	if not error:
-		try:
-			if urlData == None:
-				# open local file
-				imgOrig = Image.open(inputFile)
-			else:
-				# open file from web
-				imgOrig = Image.open(cStringIO.StringIO(urlData))
-		except:
-			error = 'Invalid image'
+	# get width/height of image
+	widthOrig = imgOrig.size[0]
+	heightOrig = imgOrig.size[1]
 
-	if not error:
-		# get width/height of image
-		widthOrig = imgOrig.size[0]
-		heightOrig = imgOrig.size[1]
+	# create tile of image
+	imgOrigResized = imgOrig.resize((tileSize, tileSize))
 
-		# create tile of image
-		imgOrigResized = imgOrig.resize((tileSize, tileSize))
+	# create new image
+	imgNew = Image.new('RGB', ((widthOrig * tileSize), (heightOrig * tileSize)))
 
-		# create new image
-		imgNew = Image.new('RGB', ((widthOrig * tileSize), (heightOrig * tileSize)))
+	# load image pixel data
+	imgOrig = imgOrig.convert('RGB')
+	pixels = imgOrig.load()
 
-		# load image pixel data
-		imgOrig = imgOrig.convert('RGB')
-		pixels = imgOrig.load()
+	# iterate through original image pixels
+	for x in range(0, widthOrig):
+		for y in range(0, heightOrig):
+			# add tile to new image
+			imgNew.paste(imgOrigResized, ((x * tileSize), (y * tileSize)))
 
-		# iterate through original image pixels
-		for x in range(0, widthOrig):
-			for y in range(0, heightOrig):
-				# add tile to new image
-				imgNew.paste(imgOrigResized, ((x * tileSize), (y * tileSize)))
+			# get pixel color from original image
+			rgb = pixels[x, y]
+			r = rgb[0]
+			g = rgb[1]
+			b = rgb[2]
 
-				# get pixel color from original image
-				rgb = pixels[x, y]
-				r = rgb[0]
-				g = rgb[1]
-				b = rgb[2]
+			# add translucent color layer over tile
+			imgColorTile = Image.new('RGBA', (tileSize, tileSize), (r, g, b, 127))
+			imgNew.paste(imgColorTile, ((x * tileSize), (y * tileSize)), imgColorTile)
 
-				# add translucent color layer over tile
-				imgColorTile = Image.new('RGBA', (tileSize, tileSize), (r, g, b, 127))
-				imgNew.paste(imgColorTile, ((x * tileSize), (y * tileSize)), imgColorTile)
-
-		# save file
-		imgNew.save(outputFile, 'PNG')
-
-if error != False:
-	print(error)
+	# save file
+	imgNew.save(outputFile, 'PNG')
+except Exception as error:
+	raise Exception('Failed to create Mosaic: {}'.format(error))
